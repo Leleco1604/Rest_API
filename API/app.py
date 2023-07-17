459364046cceca41abe7a2c2edad1862ca0598c2
Leleco1604 <leonardogbarbosamelo2003@gmail.com>
2023-07-17 13:18:17 -0300

    commit

---
+++ API/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, request
+import json
+
+app = Flask(__name__)
+
+@app.route('/<int:id>')
+def pessoa(id):
+    soma = 1 + id 
+    return jsonify ({'id' : id,'nome' : 'Leonardo', 'profissao' : 'Desenvolvedor'})
+
+
+# @app.route('/soma/<int:valor1>/<int:valor2>/')
+# def soma(valor1,valor2):
+#     return jsonify ({'soma': valor1 + valor2})
+
+
+@app.route('/soma', methods = ['POST', 'GET' ,'PUT' ])
+def soma():
+    if request.method == 'POST':
+        dados = json.loads(request.data)
+        total = sum(dados['valores'])
+        return jsonify({'soma' : total})
+    elif request.method == 'GET':
+        total = 10 + 10
+    return jsonify({'soma' : total})
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
